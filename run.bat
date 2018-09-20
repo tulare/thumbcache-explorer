dec17b2f470def463fbbfaec70b184c2af2c9b0e
tulare <tulare.paxgalactica@gmail.com>
2018-09-20 14:53:17 +0200

    Add run.bat for launching application

---
+++ run.bat
@@ -0,0 +1,8 @@
+@SETLOCAL
+@SET APP_DIR=%~dp0
+@PATH=C:\Program Files\Git\cmd;%PATH%
+@SET PIPENV_PIPFILE=%APP_DIR%\Pipfile
+@SET PIPENV_VENV_IN_PROJECT=1
+@py -m pipenv install --deploy
+@py -m pipenv run python %APP_DIR%\main.py %*
+@ENDLOCAL
