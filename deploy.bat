4d5c86c3e5cd91fa7365ff95658ee3dcfdbc9c0e
tulare <tulare.paxgalactica@gmail.com>
2018-09-24 09:56:36 +0200

    Add deploy.bat for dependencies deployment.

--- deploy.bat
+++ deploy.bat
@@ -3,5 +3,5 @@
 @PATH=C:\Program Files\Git\cmd;%PATH%
 @SET PIPENV_PIPFILE=%APP_DIR%\Pipfile
 @SET PIPENV_VENV_IN_PROJECT=1
-@py -m pipenv run python %APP_DIR%\main.py %*
+@py -m pipenv install --deploy
 @ENDLOCAL
