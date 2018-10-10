c875fe95cb0ab7a9eefd0421b19500521cd766d6
tulare <tulare.paxgalactica@gmail.com>
2018-10-10 11:00:29 +0200

    Change pipenv configuration

--- idle.bat
+++
@@ -1,7 +0,0 @@
-@SETLOCAL
-@SET APP_DIR=%~dp0
-@PATH=C:\Program Files\Git\cmd;%PATH%
-@SET PIPENV_PIPFILE=%APP_DIR%\Pipfile
-@SET PIPENV_VENV_IN_PROJECT=1
-@START "IDLE" /B /D %APP_DIR% py -m pipenv run pythonw -m idlelib %*
-@ENDLOCAL
--- pipenv.bat
+++
@@ -1,5 +0,0 @@
-@SETLOCAL
-@PATH=C:\Program Files\Git\cmd;%PATH%
-@SET PIPENV_VENV_IN_PROJECT=1
-@py -m pipenv %*
-@ENDLOCAL
