633d5962f3887e8f47bd345fb9c9997e78b0ff44
tulare <tulare.paxgalactica@gmail.com>
2023-02-15 18:53:30 +0100

    remove git path from run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 @SETLOCAL
 @SET APP_DIR=%~dp0
-@PATH=C:\Program Files\Git\cmd;%PATH%
 @SET PIPENV_PIPFILE=%APP_DIR%\Pipfile
 @SET PIPENV_VENV_IN_PROJECT=1
 @py -m pipenv run python %APP_DIR%\main.py %*
