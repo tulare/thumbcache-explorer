7a3939b679e0269403aa4540d313eba9a8cd228b
tulare <tulare.paxgalactica@gmail.com>
2024-06-20 11:33:46 +0200

    fix run(w) scripts

--- run.cmd
+++ run.cmd
@@ -1,4 +1,16 @@
 @SETLOCAL
+
+@IF EXIST .venv\Scripts\python.exe @GOTO :python_exe
+@ECHO Aucun environement virtuel avec python
+@GOTO :EOF
+
+:python_exe
+@FOR /F %%p in ('.venv\Scripts\python.exe -c "import sys; print(sys._base_executable)"') DO @(
+	SET "PYTHON_EXE=%%p"
+)
+
+:pipenv
 @SET PIPENV_VENV_IN_PROJECT=1
-@.venv\Scripts\python.exe -B %*
+@%PYTHON_EXE% -m pipenv run python %*
+
 @ENDLOCAL
--- runw.cmd
+++ runw.cmd
@@ -1,4 +1,17 @@
 @SETLOCAL
+
+@IF EXIST .venv\Scripts\python.exe @GOTO :python_exe
+@ECHO Aucun environement virtuel avec python
+@GOTO :EOF
+
+:python_exe
+@FOR /F %%p in ('.venv\Scripts\python.exe -c "import sys; print(sys._base_executable)"') DO @(
+	SET "PYTHON_EXE=%%p"
+)
+
+:pipenv
 @SET PIPENV_VENV_IN_PROJECT=1
-@START .venv\Scripts\pythonw.exe -B %*
+@SET "PYTHONW_EXE=%PYTHON_EXE:.exe=w.exe%"
+@START %PYTHONW_EXE% -m pipenv run pythonw %*
+
 @ENDLOCAL
